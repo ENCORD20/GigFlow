bb79ba10c24b060906ad12bff4a6ec3d3a12efa6
ENCORD20 <utkarshnegi680@gmail.com>
2026-01-14 16:35:22 +0530

    fixed auth error

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -8,8 +8,8 @@ const generateToken = (res, userId) => {
 
   res.cookie('jwt', token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== 'development', // Use secure cookies in production
-    sameSite: 'strict', // Prevent CSRF attacks
+    secure: true, // Must be true for sameSite: 'none'
+    sameSite: 'none', // Allow cross-site cookie
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
 };
